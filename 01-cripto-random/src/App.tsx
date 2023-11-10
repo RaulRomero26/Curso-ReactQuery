53b5be1792843bb49486e86258492aa1b8a28204
RaulRomero26 <link_2642@hotmail.com>
2023-11-10 10:47:07 -0600

    update: peticiones fecth sin  reactquery

--- 01-cripto-random/src/App.tsx
+++ 01-cripto-random/src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useReducer } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const getRndomNumberFromApi = async():Promise<number> => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
+  const numberString = await res.text();
+  return +numberString;
 }
 
-export default App
+
+export const App = () => {
+  const [number, setNumber] = useState<number>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>();
+  const [key, forceRefetch] = useReducer( (x) => x+1 , 0 );
+
+  useEffect(() => {
+    setIsLoading(true);
+    getRndomNumberFromApi()
+      .then(setNumber)
+      .catch(error => setError(error.message))
+  }, [key])
+
+  useEffect(() => {
+    if(number) setIsLoading(false);
+  }, [number])
+  
+  useEffect(() => {
+    if( error ) setIsLoading(false);
+  }, [error])
+
+  return (
+    <div className='App App-header'>
+      { 
+        isLoading 
+        ? ( <h2>Cargando...</h2> ) 
+        : ( <h2>Numero aleatorio: {number} </h2> )
+      }
+
+      {
+        !isLoading && error  && (<h3>{error}</h3>)
+      }
+      
+      <button onClick={ forceRefetch } disabled={isLoading}>
+        {
+          isLoading ? '...' : 'Nuevo número'
+        }
+      </button>
+
+    </div>
+  );
+}
