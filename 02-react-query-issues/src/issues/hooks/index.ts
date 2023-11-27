62c4ecdd096317b0cb0684cb10a43d53f0c0c771
RaulRomero26 <link_2642@hotmail.com>
2023-11-27 11:24:23 -0600

    fix: error useIssuesInfinite not working

--- 02-react-query-issues/src/issues/hooks/index.ts
+++ 02-react-query-issues/src/issues/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useIssue } from './useIssue';
 export { useIssuees } from "./useIssuees";
 export { useLabels } from "./useLabels";
+export { useIssuesInfinite } from "./useIssuesInfinite";
