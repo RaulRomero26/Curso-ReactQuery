85282be0001d25e5a12b1f5f79641a24e480db3e
RaulRomero26 <link_2642@hotmail.com>
2023-11-30 17:29:59 -0600

    update: start mutaations

--- 03-react-query-store/src/products/services/actions.ts
+++ 03-react-query-store/src/products/services/actions.ts
@@ -29,3 +29,19 @@ export const getProductById = async ( id: number):Promise<Product> => {
     const {data} = await productsApi.get<Product>(`/products/${id}`);
     return data;
 }
+
+export interface ProductLike {
+    id?:         number;
+    title:       string;
+    price:       number;
+    description: string;
+    category:    string;
+    image:       string;
+}
+
+export const creareProduct = async( product: ProductLike) => {
+    await sleep(2);
+
+    const {data} = await productsApi.post<Product>('/products',product)
+    return data;
+}
