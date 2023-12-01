105b185d7ac1f0c56531e16cdc31ae86511be16c
RaulRomero26 <link_2642@hotmail.com>
2023-12-01 15:51:29 -0600

    update: on error

--- 03-react-query-store/src/products/hooks/useProductMutation.jsx
+++ 03-react-query-store/src/products/hooks/useProductMutation.jsx
@@ -7,19 +7,61 @@ export const useProductMutation = () => {
   
     const mutation = useMutation({
         mutationFn: productActions.creareProduct,
-        onSuccess: (data) => {
+        onMutate: (product) => {
+            console.log('Mutando optimistic update')
+            //se crea el producto optimista
+            const optimisticProduct = {id: Math.random(), ...product}
+            //afectar el cache
+            queryClient.setQueryData(//aqui se pondria el tipado pero me da error
+                ["products", { "filterKey": product.category }],
+                (old) =>{
+                    if(!old) return [optimisticProduct]
+
+                    return [old, ...optimisticProduct]
+                }
+              );
+
+            return {
+                optimisticProduct
+            }
+        },
+        onSuccess: (product, variables, context) => {
             // queryClient.invalidateQueries(
             //     ["products",{"filterKey":data.category}]
             // )
+
+            queryClient.removeQueries(
+                ["product",context?.optimisticProduct.id]
+            );
             queryClient.setQueryData(//aqui se pondria el tipado pero me da error
-                ["products", { "filterKey": data.category }],
-                (old) =>
-                  old
-                    ? {...old,product}
-                    : old
+                ["products", { "filterKey": product.category }],
+                (old) => {
+                    if(!old) return[product];
+
+                    return old.map(cacheProduct => {
+                        return cacheProduct.id === context?.optimisticProduct.id ? product : cacheProduct;
+                    })
+                }
               );
 
         },
+
+        onError: (error, variables, context ) => {
+            queryClient.removeQueries(
+                ["product",context?.optimisticProduct.id]
+            );
+
+            queryClient.setQueryData(//aqui se pondria el tipado pero me da error
+            ["products", { "filterKey": variables.category }],
+            (old) => {
+                if(!old) return[];
+
+                return old.filter(cacheProduct => {
+                    return cacheProduct.id !== context?.optimisticProduct.id 
+                })
+            }
+          );
+        }
       })
   
   
